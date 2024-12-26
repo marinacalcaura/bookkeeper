c7c98c134b88b01016cad8cbedc745084731f0b1
marina <calcauram@gmail.com>
2024-12-26 19:12:13 +0100

    test BookKeeper first iter

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeeperTest.java
@@ -0,0 +1,380 @@
+package org.apache.bookkeeper.client;
+
+import org.apache.bookkeeper.client.api.LedgerMetadata;
+import org.apache.bookkeeper.conf.ServerConfiguration;
+import org.apache.bookkeeper.util.LocalBookKeeper;
+import org.apache.bookkeeper.versioning.Versioned;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+
+@RunWith(value = Enclosed.class)
+public class BookKeeeperTest {
+
+    @RunWith(value = Parameterized.class)
+    public static class CreateLedgerValidTest {
+
+        private LocalBookKeeper bookKeeper;
+        private BookKeeper client;
+        private int ensembleSize;
+        private int writeQuorumSize;
+        private int ackQuorumSize;
+        private BookKeeper.DigestType digestType;
+        private byte[] passwd;
+        private Map<String,byte[]> customMetadata;
+
+        public CreateLedgerValidTest(int ensembleSize, int writeQuorumSize, int ackQuorumSize,BookKeeper.DigestType digestType, byte[] passwd, Map<String,byte[]> customMetadata)  {
+            this.ensembleSize=ensembleSize;
+            this.writeQuorumSize=writeQuorumSize;
+            this.ackQuorumSize=ackQuorumSize;
+            this.digestType=digestType;
+            this.passwd=passwd;
+            this.customMetadata=customMetadata;
+        }
+        @Parameterized.Parameters
+        public static Collection configure(){
+            Map<String, byte[]> nonEmptyMetadata = new HashMap<>();
+            nonEmptyMetadata.put("myMetadata", "MyCustomMetadata".getBytes());
+
+            return Arrays.asList(new Object[][] {
+
+                    {3, 2, 1, BookKeeper.DigestType.MAC, new byte[6], nonEmptyMetadata},
+                    {2, 2, 1, BookKeeper.DigestType.CRC32, new byte[6], null},
+
+
+                    {2, 1, 1, BookKeeper.DigestType.MAC, new byte[6], null},
+                    {1, 1, 1, BookKeeper.DigestType.CRC32, new byte[0], new HashMap<String, byte[]>()},
+
+                    {2, 1, 0, BookKeeper.DigestType.DUMMY, new byte[0], new HashMap<String, byte[]>()},
+                    {1, 1, 0, BookKeeper.DigestType.MAC, new byte[6], null},
+
+                    {1, 0, 0, BookKeeper.DigestType.MAC, new byte[6], null},
+                    {0, 0, 0, BookKeeper.DigestType.DUMMY, new byte[6], nonEmptyMetadata},
+
+                    {1, 0, -1, BookKeeper.DigestType.MAC, new byte[6], null},
+                    {0, 0, -1, BookKeeper.DigestType.CRC32C, new byte[6], nonEmptyMetadata},
+
+                    {0, -1, -1, BookKeeper.DigestType.MAC, new byte[6], null},
+
+
+            });
+        }
+
+
+        @Before
+        public void startServer() throws Exception {
+            ServerConfiguration configuration = new ServerConfiguration();
+            configuration.setAllowLoopback(true);
+            bookKeeper = LocalBookKeeper.getLocalBookies("127.0.0.1", 34567, 3, true, configuration);
+            bookKeeper.start();
+            client = new BookKeeper("127.0.0.1:34567");
+        }
+
+        @After
+        public void closeServer() throws Exception {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close BookKeeper client: " + e.getMessage());
+                }
+            }
+            if (bookKeeper != null) {
+                try {
+                    bookKeeper.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close LocalBookKeeper: " + e.getMessage());
+                }
+            }
+            Thread.sleep(500);
+        }
+
+        /*Il test verifica la corretta creazione di un ledger utilizzando i parametri forniti,
+        controllando che i metadati del ledger creato siano consistenti con i parametri di input.
+        */
+
+        @Test
+        public void shouldCreateValidLedger() throws BKException, InterruptedException {
+
+            LedgerHandle createdLedger = client.createLedger(ensembleSize, writeQuorumSize, ackQuorumSize, digestType, passwd, customMetadata);
+            LedgerMetadata ledgerMetadata = createdLedger.getLedgerMetadata();
+
+            //verifico se la password nel ledger corrisponde a quella fornita
+            boolean isPasswordValid = ledgerMetadata.getPassword().length == passwd.length;
+            for (int index = 0; index < passwd.length; index++) {
+                if (ledgerMetadata.getPassword()[index] != passwd[index]) {
+                    isPasswordValid = false;
+                    break;
+                }
+            }
+
+            //confronta i metadati personalizzati, se presenti
+            boolean areCustomMetadataValid = (customMetadata == null || ledgerMetadata.getCustomMetadata().equals(customMetadata));
+
+            //verifica che i parametri dell'ledger siano corretti
+            boolean isLedgerValid = ledgerMetadata.getEnsembleSize() == ensembleSize
+                    && ledgerMetadata.getWriteQuorumSize() == writeQuorumSize
+                    && ledgerMetadata.getAckQuorumSize() == ackQuorumSize
+                    && isPasswordValid
+                    && areCustomMetadataValid;
+            Assert.assertTrue("Ledger creation failed due to invalid parameters", isLedgerValid);
+        }
+
+    }
+
+    @RunWith(value = Parameterized.class)
+    public static class CreateLedgerInvalidTest {
+
+
+        private LocalBookKeeper bookKeeper;
+        private BookKeeper client;
+        private int ensembleSize;
+        private int writeQuorumSize;
+        private int ackQuorumSize;
+        private BookKeeper.DigestType digestType;
+        private byte[] passwd;
+        private Map<String,byte[]> customMetadata;
+
+        public CreateLedgerInvalidTest(int ensembleSize, int writeQuorumSize, int ackQuorumSize,BookKeeper.DigestType digestType, byte[] passwd, Map<String,byte[]> customMetadata)  {
+            this.ensembleSize=ensembleSize;
+            this.writeQuorumSize=writeQuorumSize;
+            this.ackQuorumSize=ackQuorumSize;
+            this.digestType=digestType;
+            this.passwd=passwd;
+            this.customMetadata=customMetadata;
+        }
+
+        @Parameterized.Parameters
+        public static Collection configure(){
+            return Arrays.asList(new Object[][] {
+                    //{1, 2, 1, BookKeeper.DigestType.MAC, new byte[6], null}
+                    //{0, 1, 1, BookKeeper.DigestType.DUMMY, new byte[6], null},
+                    {1, 0, 1, BookKeeper.DigestType.MAC, new byte[6], null},
+                    {0, 0, 1, BookKeeper.DigestType.CRC32, new byte[6], null},
+                    //{-1, 0, 1, BookKeeper.DigestType.MAC, new byte[6], null},
+                    //{0, 1, 0, BookKeeper.DigestType.CRC32C, new byte[6], null},
+                    //{-1, 0, 0, BookKeeper.DigestType.MAC, new byte[0], null},
+                    {0, -1, 0, BookKeeper.DigestType.DUMMY, new byte[6], null},
+                    {-1, -1, 0, BookKeeper.DigestType.CRC32C, new byte[6], null},
+                    //{-2, -1, 0, BookKeeper.DigestType.MAC, new byte[0], null},
+                    //{-1, 0, -1, BookKeeper.DigestType.CRC32, new byte[6], null},
+                    {-1, -1, -1, BookKeeper.DigestType.MAC, new byte[0], null},
+                    //{-2, -1, -1, BookKeeper.DigestType.DUMMY, new byte[6], null},
+                    {-1, -2, -1, BookKeeper.DigestType.MAC, new byte[6], null},
+                    {-2, -2, -1, BookKeeper.DigestType.MAC, new byte[6], new HashMap<String, byte[]>()},
+                   // {-3, -2, -1, BookKeeper.DigestType.CRC32, new byte[6], null}
+            });
+        }
+        @Before
+        public void startServer() throws Exception {
+            ServerConfiguration configuration = new ServerConfiguration();
+            configuration.setAllowLoopback(true);
+            bookKeeper = LocalBookKeeper.getLocalBookies("127.0.0.1", 34567, 3, true, configuration);
+            bookKeeper.start();
+            client = new BookKeeper("127.0.0.1:34567");
+        }
+
+        @After
+        public void closeServer() throws Exception {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close BookKeeper client: " + e.getMessage());
+                }
+            }
+            if (bookKeeper != null) {
+                try {
+                    bookKeeper.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close LocalBookKeeper: " + e.getMessage());
+                }
+            }
+            Thread.sleep(500);
+        }
+
+        /*verifica se il metodo createLedger lancia un'eccezione quando viene
+        invocato con parametri non validi
+         */
+        @Test
+        public void testCreateLedgerException() throws BKException, InterruptedException {
+            boolean exceptionThrown = false;
+            try {
+                client.createLedger(ensembleSize, writeQuorumSize, ackQuorumSize, digestType, passwd, customMetadata);
+            } catch (Exception e) {
+                exceptionThrown = true;
+            }
+            Assert.assertTrue(exceptionThrown);
+        }
+
+    }
+
+    @RunWith(value = Parameterized.class)
+    public static class OpenLedgerTest {
+
+
+        private LocalBookKeeper bookKeeper;
+        private BookKeeper client;
+        private BookKeeper.DigestType digestType;
+        private byte[] passwd;
+
+        public OpenLedgerTest(BookKeeper.DigestType digestType, byte[] passwd) {
+            this.digestType = digestType;
+            this.passwd = passwd;
+        }
+
+        @Parameterized.Parameters
+        public static Collection configure() {
+            return Arrays.asList(new Object[][]{
+                    {BookKeeper.DigestType.MAC, "test_pwd".getBytes()},
+                    {BookKeeper.DigestType.CRC32, new byte[]{}}
+            });
+        }
+
+        @Before
+        public void startServer() throws Exception {
+            ServerConfiguration configuration = new ServerConfiguration();
+            configuration.setAllowLoopback(true);
+            bookKeeper = LocalBookKeeper.getLocalBookies("127.0.0.1", 34567, 3, true, configuration);
+            bookKeeper.start();
+            client = new BookKeeper("127.0.0.1:34567");
+        }
+
+        @After
+        public void closeServer() throws Exception {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close BookKeeper client: " + e.getMessage());
+                }
+            }
+            if (bookKeeper != null) {
+                try {
+                    bookKeeper.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close LocalBookKeeper: " + e.getMessage());
+                }
+            }
+            Thread.sleep(500);
+        }
+
+
+        @Test
+        public void testOpenLedger() throws BKException, InterruptedException {
+
+            LedgerHandle handle = client.createLedger(this.digestType, this.passwd);
+
+            String testEntry = "test_pwd";
+            byte[] entryValue = testEntry.getBytes(StandardCharsets.UTF_8);
+
+            handle.addEntry(entryValue);
+
+            long ledgerId = handle.getId();
+            handle.close();
+
+            LedgerHandle openedHandle = client.openLedger(ledgerId, this.digestType, this.passwd);
+
+            try {
+                LedgerEntry entry = openedHandle.readLastEntry();
+                byte[] actualEntry = entry.getEntry();
+
+                Assert.assertArrayEquals("The ledger entry is not as expected", entryValue, actualEntry);
+            } catch (Exception e) {
+                Assert.fail("Failed to read the expected entry from the ledger: " + e.getMessage());
+            }
+        }
+    }
+
+    @RunWith(value = Parameterized.class)
+    public static class OpenLedgerExcTest {
+
+
+        private LocalBookKeeper bookKeeper;
+        private BookKeeper client;
+        private BookKeeper.DigestType digestType;
+        private byte[] passwd;
+
+        public OpenLedgerExcTest(BookKeeper.DigestType digestType, byte[] passwd) {
+            this.digestType = digestType;
+            this.passwd = passwd;
+        }
+
+        @Parameterized.Parameters
+        public static Collection configure() {
+            return Arrays.asList(new Object[][]{
+                    {BookKeeper.DigestType.CRC32C, "test_pwd".getBytes()},
+                    {BookKeeper.DigestType.MAC, new byte[]{}},
+            });
+        }
+
+        @Before
+        public void startServer() throws Exception {
+            ServerConfiguration configuration = new ServerConfiguration();
+            configuration.setAllowLoopback(true);
+            bookKeeper = LocalBookKeeper.getLocalBookies("127.0.0.1", 34567, 3, true, configuration);
+            bookKeeper.start();
+            client = new BookKeeper("127.0.0.1:34567");
+        }
+
+        @After
+        public void closeServer() throws Exception {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close BookKeeper client: " + e.getMessage());
+                }
+            }
+            if (bookKeeper != null) {
+                try {
+                    bookKeeper.close();
+                } catch (Exception e) {
+                    System.err.println("Failed to close LocalBookKeeper: " + e.getMessage());
+                }
+            }
+            Thread.sleep(500);
+        }
+
+
+        @Test
+        public void testOpenLedgerBadPassword() throws BKException, InterruptedException {
+            // Caso di password errata
+            LedgerHandle handle = client.createLedger(this.digestType, "bad_pwd".getBytes(StandardCharsets.UTF_8));
+
+            try {
+                client.openLedger(handle.getId(), this.digestType, this.passwd);
+                Assert.fail("Expected BKException due to bad password");
+            } catch (BKException e) {
+                Assert.assertTrue("BKException is expected due to wrong password", true);
+            }
+        }
+
+        @Test
+        public void testOpenLedgerNonExistent() throws BKException, InterruptedException {
+            // Caso di ledger inesistente
+            try {
+
+                client.openLedger(-1, this.digestType, this.passwd);
+                Assert.fail("Expected BKException due to non-existent ledger");
+            } catch (BKException e) {
+                Assert.assertTrue("BKException is expected due to non-existent ledger", true);
+            }
+        }
+
+    }
+
+
+}
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/net/NetworkTopologyImplTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/net/NetworkTopologyImplTest.java
@@ -10,9 +10,6 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.fail;
-
-
 // Class for the test
 @RunWith(value = Enclosed.class)
 public class NetworkTopologyImplTest {
@@ -24,26 +21,23 @@ public class NetworkTopologyImplTest {
         private boolean expectException;
         private Class<? extends Exception> expectedExceptionType;
 
-        private NetworkTopologyImpl sut; // SUT = System Under Test
+        private NetworkTopologyImpl sut;
 
-        // Constructor for parameterized test
         public AddNodeTest(Node node, boolean expectException, Class<? extends Exception> expectedExceptionType) {
             this.node = node;
             this.expectException = expectException;
             this.expectedExceptionType = expectedExceptionType;
         }
 
-        // Define the test parameters (inputs)
         @Parameterized.Parameters
         public static Collection<Object[]> data() {
             return Arrays.asList(new Object[][] {
 
-                    {new NodeBase("127.0.0.1:5555", "/root"), false, null},  // LeafNode, valid case, no exception
-                    {new NetworkTopologyImpl.InnerNode("127.0.0.1:4001", "/root/inner"), true, IllegalArgumentException.class }, // InnerNode, should throw IllegalArgumentException
-                    {null, false, null},  // null node, should just return (no exception)
-                    //{new NodeBase("127.0.0.1:4002", "/root/127.0.0.1:3999"), true, IllegalArgumentException.class},  // Invalid topology node, should throw InvalidTopologyException
-                    //alla seconda iterazione
-                    {new NodeBase("127.0.0.1:4002", "/root/first"), true, NetworkTopologyImpl.InvalidTopologyException.class },  // Invalid topology node, should throw InvalidTopologyException
+                    {new NodeBase("127.0.0.1:5555", "/root"), false, null},
+                    {new NetworkTopologyImpl.InnerNode("127.0.0.1:4001", "/root/inner"), true, IllegalArgumentException.class},
+                    {null, false, null},
+                    //{new NodeBase("127.0.0.1:4002", "/root/127.0.0.1:3999"), true, IllegalArgumentException.class},
+                    {new NodeBase("127.0.0.1:4002", "/root/first"), true, NetworkTopologyImpl.InvalidTopologyException.class},
 
             });
         }
@@ -58,6 +52,7 @@ public class NetworkTopologyImplTest {
                 String rack = node.getNetworkLocation() + "/rack/127.0.0.1:4003";
                 sut.add(new NodeBase(rack));
             }
+
             /*if (expectException && expectedExceptionType == IllegalArgumentException.class) {
                 sut.add(new NodeBase("127.0.0.1:3999", "/root"));
             }*/
@@ -71,13 +66,13 @@ public class NetworkTopologyImplTest {
                     sut.add(node);
                 });
             } else {
-                // In case we don't expect an exception, handle the `null` case inside this block
+
                 if (node == null) {
-                    sut.add(node);  // Add a null node, should not throw an exception
-                    assertFalse(sut.contains(node));  // The node should not be contained (as it's null)
+                    sut.add(node);
+                    assertFalse(sut.contains(node));
                 } else {
-                    sut.add(node);  // Add the non-null node
-                    assertTrue(sut.contains(node));  // Verify that the node has been added successfully
+                    sut.add(node);
+                    assertTrue(sut.contains(node));
                 }
             }
         }
@@ -90,23 +85,23 @@ public class NetworkTopologyImplTest {
         private boolean expectException;
         private Class<? extends Exception> expectedExceptionType;
 
-        private NetworkTopologyImpl sut; // SUT = System Under Test
+        private NetworkTopologyImpl sut;
 
-        // Constructor for parameterized test
+
         public RemoveNodeTest(Node node, boolean expectException, Class<? extends Exception> expectedExceptionType) {
             this.node = node;
             this.expectException = expectException;
             this.expectedExceptionType = expectedExceptionType;
         }
 
-        // Define the test parameters (inputs)
+
         @Parameterized.Parameters
         public static Collection<Object[]> data() {
             return Arrays.asList(new Object[][] {
 
-                    {new NodeBase("127.0.0.1:5555", "/root"), false, null},  // LeafNode, valid case, no exception
-                    {new NetworkTopologyImpl.InnerNode("127.0.0.1:5556", "/root/inner"), true, IllegalArgumentException.class }, // InnerNode, should throw IllegalArgumentException
-                    {null, false, null},  // null node, should just return (no exception)
+                    {new NodeBase("127.0.0.1:5555", "/root"), false, null},
+                    {new NetworkTopologyImpl.InnerNode("127.0.0.1:5556", "/root/inner"), true, IllegalArgumentException.class },
+                    {null, false, null},
 
             });
         }
@@ -134,4 +129,159 @@ public class NetworkTopologyImplTest {
             }
         }
     }
+
+    @RunWith(value = Parameterized.class)
+    public static class TopologyTest {
+
+        private Node node1;
+        private Node node2;
+        private boolean expectException;
+        private Class<? extends Exception> expectedExceptionType;
+
+        private NetworkTopologyImpl sut;
+
+
+        public TopologyTest(Node node1, Node node2, boolean expectException, Class<? extends Exception> expectedExceptionType) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.expectException = expectException;
+            this.expectedExceptionType = expectedExceptionType;
+        }
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][] {
+
+                    {new NodeBase("127.0.0.1:4001", "/root/rack1"),new NodeBase("127.0.0.1:4002", "/root/rack1"), false, null},
+                    {new NodeBase("127.0.0.1:4001", "/root/rack1"),new NodeBase("127.0.0.1:4002", "/root/rack2"), false, null},
+                    //{null, null, true, IllegalArgumentException.class},
+                    //{null,new NodeBase("127.0.0.1:4002", "/root/rack2"),true,IllegalArgumentException.class}
+
+            });
+        }
+
+        // Setup prima di ogni test
+        @Before
+        public void setUp() {
+            sut = new NetworkTopologyImpl();
+        }
+        @Test
+        public void testIsOnSameRack() {
+
+            sut.add(node1);
+            sut.add(node2);
+
+            if (expectException) {
+                assertThrows(expectedExceptionType, () -> {
+                    sut.isOnSameRack(node1,node2);
+                });
+            } else {
+
+                boolean result = sut.isOnSameRack(node1,node2);
+                // Aggiungi la stampa per visualizzare le locazioni di rete
+                System.out.println("Node 1 Location: " + node1.getNetworkLocation());
+                System.out.println("Node 2 Location: " + node2.getNetworkLocation());
+                boolean expected = node1.getNetworkLocation().equals(node2.getNetworkLocation());
+                // Stampare anche il risultato previsto e ottenuto
+                System.out.println("Expected result: " + expected);
+                System.out.println("Actual result: " + result);
+                Assert.assertEquals(expected, result);
+
+            }
+        }
+    }
+
+    @RunWith(value = Parameterized.class)
+    public static class GetDistanceTest {
+
+        private Node node1;
+        private Node node2;
+        private boolean expectException;
+        private Class<? extends Exception> expectedExceptionType;
+
+        private NetworkTopologyImpl sut;
+
+
+        public GetDistanceTest(Node node1, Node node2, boolean expectException, Class<? extends Exception> expectedExceptionType) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.expectException = expectException;
+            this.expectedExceptionType = expectedExceptionType;
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][] {
+
+                    {new NodeBase("127.0.0.1:4001", "/root/rack1"),new NodeBase("127.0.0.1:4001", "/root/rack1"), false, null},
+                    {new NodeBase("127.0.0.1:4001", "/root/rack1"),new NodeBase("127.0.0.1:4002", "/root/rack2"), false, null},
+                    {new NodeBase("127.0.0.1:4001", "/root/rack1"),new NodeBase("127.0.0.1:4002", "/root/rack1"), false, null},
+                    //{null, null, true, NullPointerException.class},
+                    {null,new NodeBase("127.0.0.1:4002", "/root/rack2"),true,NullPointerException.class},
+                    {new NodeBase("127.0.0.1:4002", "/root/rack2"),null,true,NullPointerException.class}
+
+            });
+        }
+
+        // Setup prima di ogni test
+        @Before
+        public void setUp() {
+            sut = new NetworkTopologyImpl();
+        }
+
+        private int calculateExpectedDistance(Node node1, Node node2){
+
+            System.out.println("Comparing nodes:");
+            System.out.println("Node 1 address: " + node1.getName() + ", location: " + node1.getNetworkLocation());
+            System.out.println("Node 2 address: " + node2.getName() + ", location: " + node2.getNetworkLocation());
+
+            if(node1 == null || node2 == null){
+                return Integer.MAX_VALUE;
+            }
+
+            if(node1.equals(node2)){
+                return 0;
+            }
+
+            Node parent1 = node1.getParent();
+            Node parent2 = node2.getParent();
+            int distance = 0;
+
+            while(parent1 != null && parent2 != null && parent1 != parent2) {
+
+                parent1 = parent1.getParent();
+                parent2 = parent2.getParent();
+                distance += 2;
+            }
+
+            if(parent1 == parent2) {
+                distance += 2; //
+            }
+
+            return distance;
+        }
+        @Test
+        public void testGetDistance() {
+            sut.add(node1);
+            sut.add(node2);
+
+            if (expectException) {
+                assertThrows(expectedExceptionType, () -> {
+                    sut.getDistance(node1,node2);
+                });
+            } else {
+                int result = sut.getDistance(node1,node2);
+                int expected = calculateExpectedDistance(node1,node2);
+
+                // Stampare anche il risultato previsto e ottenuto
+                System.out.println("Expected result: " + expected);
+                System.out.println("Actual result: " + result);
+
+                Assert.assertEquals(expected,result);
+
+            }
+        }
+    }
+
 }
